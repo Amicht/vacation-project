1973894d02118e1631615870dea53bd5d5c00774
Amicht <vicolicht@gmail.com>
2022-06-24 09:12:03 +0300

    interfaces

---
+++ client/src/interface/UserI.ts
@@ -0,0 +1,8 @@
+export default interface UserI{
+    id: number,
+    name: string,
+    last_name: string,
+    username: string,
+    role: 1 | 2
+};
+
---
+++ client/src/interface/vacationI.ts
@@ -0,0 +1,12 @@
+interface VacationI{
+    id: number,
+    description: string,
+    destination: string,
+    picture: string,
+    from_date: string,
+    to_date: string,
+    price: number,
+    follow: number
+};
+
+export default VacationI;
