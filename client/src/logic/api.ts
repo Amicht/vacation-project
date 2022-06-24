ee2456d8abf7725217ff69d96a4c2e6e15336412
Amicht <vicolicht@gmail.com>
2022-06-24 09:04:43 +0300

    api logic

---
+++ client/src/logic/api.ts
@@ -0,0 +1,31 @@
+
+const END_POINT = {
+    login: '/api/user/login',
+    register: '/api/user/register',
+    vacation: '/api/vacation'
+};
+
+const postData = (end_point: string, data:object) => fetch(end_point, {
+    method: "POST",
+    headers: {"Content-type": "Application/json"},
+    body: JSON.stringify(data)
+});
+
+export const setToken = (token:string) => window.sessionStorage.setItem("user-jwt", JSON.stringify(token));
+
+const getToken = () => {
+    const token =  window.sessionStorage.getItem("user-jwt") || "[]";
+    return JSON.parse(token);
+};
+
+const fetchData = (end_point: string) => fetch(end_point, {
+    method: "GET",
+    headers: {"Content-type": "Application/json", authorization: "Bearer " + getToken()},
+});
+export const getVacations = () => fetchData(END_POINT.vacation).then(res => res.json())
+
+export const login  = (data:object) => postData(END_POINT.login, data)
+    .then(res => res.json()).then(setToken);
+export const register  = (data:object) => postData(END_POINT.register, data)
+    .then(res => res.json()).then(setToken);
+
