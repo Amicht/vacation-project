1e9be25afcfc870c72fa04b8f5ed13647e68d57e
Amicht <vicolicht@gmail.com>
2022-06-26 16:44:27 +0300

    express routes deleted

--- server/controllers/vacationCtrl.js
+++ server/controllers/vacationCtrl.js
@@ -1,47 +1,19 @@
 const express = require('express');
 const bl = require('../bl/vacation-queries');
-const {authAdmin, authUser} = require('../middleware/auth-middleware');
+const { authUser} = require('../middleware/auth-middleware');
 const vacationCtrl = express.Router();
 
 
 vacationCtrl.get('/', authUser, (req,res, next)=> {
     try{
-        bl.getAllVacations(req.body.user ,vacations => res.json({vacations,user: req.body.user}));
+        if(req.body.user.role===1){
+            bl.getAllVacations(req.body.user ,vacations => res.json({vacations,user: req.body.user}));
+        }
+        if(req.body.user.role===2){
+            bl.getAdminVacations(vacations => res.json({vacations,user: req.body.user}));
+        }
     }
     catch(err){ next(err) }
 });
-
-vacationCtrl.post('/', authAdmin, (req,res)=>{
-    try{
-        bl.addVacation(req.body, data => {
-            if(!data)return res.status(400).send("bad request");
-            res.json(data);
-        });
-    }
-    catch(err){ next(err)};
-});
-
-vacationCtrl.put('/:id', authAdmin, (req,res)=>{
-    req.body.id = +req.params.id;
-    
-    try{
-        bl.updateVacation(req.body, data => {
-            if(data.affectedRows === 0) return res.status(404).send("id not found");
-            res.json(data);
-        });
-    }
-    catch(err){ next(err)};
-});
-
-vacationCtrl.delete('/:id',authAdmin, (req,res,next)=> {
-    const vacation_id = +req.params.id;
-    try{
-        bl.deleteVacation(vacation_id, () => {
-            res.status(204).send();
-        });
-    }
-    catch(err) {next(err)}
-});
-
 
 module.exports = vacationCtrl;
