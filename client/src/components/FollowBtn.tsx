703f13c7ef26dc52d3a8d0f0c7ffa01ad12d7206
Amicht <vicolicht@gmail.com>
2022-06-26 07:20:57 +0300

    follow btn added

--- client/src/components/FollowBtn.tsx
+++ client/src/components/FollowBtn.tsx
@@ -1,14 +1,31 @@
+import { useContext } from "react"
+import { SocketCtxt, UserCtxt } from "../App"
 
 interface btnI{
-    follow:number
+    follow:number,
+    vacId: number
 };
 
+
 const FollowBtn = (props:btnI) => {
+  const {user} = useContext(UserCtxt);
+  const mySocket = useContext(SocketCtxt);
+
+  const onFollowClick = ()=> {
+    if(props.follow){
+      mySocket.unfollow(user, props.vacId);
+    }
+    else{
+      mySocket.follow(user, props.vacId);
+    }
+  }
     
   return (
     <div className="row">
         <span className="col"></span>
-        <button style={{borderRadius:"50%",fontWeight:"bolder",borderColor:"black"}} className={props.follow?"btn btn-warning col-2": "btn btn-light col-2"}>f</button>
+        <button style={{borderRadius:"50%",fontWeight:"bolder",borderColor:"black"}} 
+        className={props.follow?"btn btn-warning col-2": "btn btn-light col-2"}
+        onClick={onFollowClick}>f</button>
     </div>
   )
 }
